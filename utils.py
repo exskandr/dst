c61ba139357bb1cdbad3f8e3bff4e4457fb04b26
exskandr <exskandr@gmail.com>
2024-03-28 18:55:52 +0200

    add draw_object_for_tracking

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def draw_tracked_objects(frame, tracked_objects):
 
     Args:
         frame: Numpy image array.
-        tracked_objects: List of monitored objects.
+        tracked_objects: Dict of monitored objects.
     """
 
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -24,14 +24,6 @@ def draw_tracked_objects(frame, tracked_objects):
         cv2.putText(frame, text, (10, 30), font, font_scale, font_color, thickness)
         return
 
-    # # Display the ID of each tracked object(for list)
-    # for i, track_id in enumerate(tracked_objects):
-    #     font_scale = 0.9
-    #     text = f"DRONE: {i + 1} - ID: {track_id}"
-    #     cv2.putText(frame, text, (x, y + 35 * i), font, font_scale, font_color, thickness)
-    #
-    #     # Frame display
-    #     cv2.rectangle(frame, (x-10, y-30), (x+280, y+10+35*i), (255, 255, 0), thickness)
 
     # Display the ID of each tracked object(for dict)
     for key, value in tracked_objects.items():
@@ -43,4 +35,25 @@ def draw_tracked_objects(frame, tracked_objects):
         cv2.rectangle(frame, (x - 10, y - 30), (x + 280, y + 10 + 35 * (key-1)), (255, 255, 0), thickness)
 
 
-# work with adding detect object to some position
+# draw in uaw
+def draw_object_for_tracking(frame, tracked_objects, position):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.6
+    font_color = (0, 255, 0)
+    thickness = 2
+    x, y = 20, 40
+
+    if not tracked_objects:
+        # Displaying the text "No objects found"
+        text = "No objects selected!!!"
+        font_color = (0, 0, 255)
+        font_scale = 0.7
+        cv2.putText(frame, text, (10, 30), font, font_scale, font_color, thickness)
+        return
+
+    text = f"DRONE: {position} - ID: {tracked_objects[position]}"
+    font_scale = 0.9
+    cv2.putText(frame, text, (x, y), font, font_scale, font_color, thickness)
+
+    # Frame display
+    cv2.rectangle(frame, (x - 10, y - 30), (x + 280, y + 10), (255, 255, 0), thickness)
